63e2eb94378d4989be612210b68b00930750caab
0002693 <jiangzaiming@mininglamp.com>
2019-11-19 16:33:21 +0800

    修改

--- consul-one/src/main/java/com/ming/blog/controller/TestController.java
+++ consul-one/src/main/java/com/ming/blog/controller/TestController.java
@@ -1,9 +1,16 @@
 package com.ming.blog.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author Jiang Zaiming
@@ -14,8 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
+    @Resource
+    private RestTemplate restTemplate;
+    @Resource
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/test1")
     public String test1(String word) {
+        List<ServiceInstance> instances = discoveryClient.getInstances("consul-member");
+        System.out.println(instances);
         return word + "one";
     }
 
--- consul-two/src/main/java/com/ming/blog/ConsulTwoApplication.java
+++ consul-two/src/main/java/com/ming/blog/ConsulTwoApplication.java
@@ -4,6 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author Jiang Zaiming
@@ -17,6 +20,14 @@ public class ConsulTwoApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsulTwoApplication.class, args);
         log.info("consul two client start");
+    }
+
+    // 解决RestTemplate 找不到原因 应该把restTemplate注册到SpringBoot容器中@bean
+    // 如果使用rest方式以别名方式进行调用必须依赖ribbon负载均衡
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
