07e2f1941658b5bf9c4d25b5654ed87ccf4868be
0002693 <jiangzaiming@mininglamp.com>
2019-11-20 10:46:12 +0800

    初始化配置中心的模块

---
+++ config-center-tzree/src/main/java/com/ming/blog/ConfigCenterThreeApp.java
@@ -0,0 +1,17 @@
+package com.ming.blog;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author Jiang Zaiming
+ * @date 2019/11/20 10:45 上午
+ */
+@Slf4j
+@EnableDiscoveryClient
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigCenterThreeApp {
+}
